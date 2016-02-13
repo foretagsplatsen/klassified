8e127c7230dfd74a1eb4ef25550776444bd9bd07
Nicolas Petton <nicolas@petton.fr>
2016-02-13 21:15:22 +0100

    untabify test/extensionTest.js

--- test/extensionTest.js
+++ test/extensionTest.js
@@ -6,56 +6,56 @@ define(function(require) {
     suite("extension");
 
     test('Can extend Object', function() {
-		object.extend(function(that, spec, my) {
-			that.isObject = function() {
-				return true;
-			};
-		});
+        object.extend(function(that, spec, my) {
+            that.isObject = function() {
+                return true;
+            };
+        });
 
-		var o = object();
+        var o = object();
 
-		assert.ok(o.isObject());
+        assert.ok(o.isObject());
     });
 
-	test('Extensions are inherited', function() {
-		object.extend(function(that, spec, my) {
-			that.isDog = function() {
-				return false;
-			};
-		});
+    test('Extensions are inherited', function() {
+        object.extend(function(that, spec, my) {
+            that.isDog = function() {
+                return false;
+            };
+        });
 
-		var animal = object.subclass(function() {});
+        var animal = object.subclass(function() {});
 
-		var a = animal();
+        var a = animal();
 
-		assert.ok(!a.isDog());
+        assert.ok(!a.isDog());
     });
 
-	test('Can extend subclasses of object', function() {
-		var animal = object.subclass(function(that, spec, my) {
-			that.isAnimal = function() {
-				return true;
-			};
-		});
+    test('Can extend subclasses of object', function() {
+        var animal = object.subclass(function(that, spec, my) {
+            that.isAnimal = function() {
+                return true;
+            };
+        });
 
-		var dog = animal.subclass(function(that, spec, my) {
-			that.isDog = function() {
-				return true;
-			};
-		});
+        var dog = animal.subclass(function(that, spec, my) {
+            that.isDog = function() {
+                return true;
+            };
+        });
 
-		animal.extend(function(that, spec, my) {
-			that.isDog = function() {
-				return false;
-			};
-		});
+        animal.extend(function(that, spec, my) {
+            that.isDog = function() {
+                return false;
+            };
+        });
 
-		var a = animal();
-		var d = dog();
+        var a = animal();
+        var d = dog();
 
-		assert.ok(a.isAnimal());
-		assert.ok(!a.isDog());
-		assert.ok(d.isAnimal());
-		assert.ok(d.isDog());
+        assert.ok(a.isAnimal());
+        assert.ok(!a.isDog());
+        assert.ok(d.isAnimal());
+        assert.ok(d.isDog());
     });
 });
