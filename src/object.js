f0c51edbcea7e80746fa22e1deeb6a7e6d344888
Nicolas Petton <nicolas@petton.fr>
2016-03-06 11:37:32 +0100

    Move the polyfill to its own file
    * src/object.js: Load polyfills.js.
    * src/polyfills.js: New file.

--- src/object.js
+++ src/object.js
@@ -1,4 +1,6 @@
-define(function(){
+define([
+	'./polyfills'
+], function(polyfills){
 
 	/**
 	 * `object` is the base class of the object model.
@@ -180,33 +182,6 @@ define(function(){
 	object.extend = function(builder) {
 		this.extensions.push(builder);
 	};
-
-	/**
-	 * Polyfill for Object.assign
-	 */
-	if (typeof Object.assign != 'function') {
-		(function () {
-			Object.assign = function (target) {
-				'use strict';
-				if (target === undefined || target === null) {
-					throw new TypeError('Cannot convert undefined or null to object');
-				}
-
-				var output = Object(target);
-				for (var index = 1; index < arguments.length; index++) {
-					var source = arguments[index];
-					if (source !== undefined && source !== null) {
-						for (var nextKey in source) {
-							if (source.hasOwnProperty(nextKey)) {
-								output[nextKey] = source[nextKey];
-							}
-						}
-					}
-				}
-				return output;
-			};
-		})();
-	}
 
 	return object;
 });
