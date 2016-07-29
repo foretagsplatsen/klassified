6dcf5c0f08981e53f2e0e3c6d126976dc6373b2e
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2016-07-29 16:10:20 +0200

    Add minification

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@ var del = require("del");
 var Server = require("karma").Server;
 
 var plugins = require("gulp-load-plugins")({
-    rename: {
-        "gulp-eslint": "eslint",
-        "gulp-shell": "shell",
-        "gulp-phantom": "phantom",
-        "gulp-strip-code": "stripCode"
-    }
+	rename: {
+		"gulp-eslint": "eslint",
+		"gulp-shell": "shell",
+		"gulp-phantom": "phantom",
+		"gulp-strip-code": "stripCode",
+		"gulp-sequence": "sequence",
+		"gulp-requirejs-optimize": "optimizer"
+	}
 });
 
 var sources = ["./src/**/*.js"];
@@ -23,41 +25,88 @@ gulp.task("default", ["lint", "test"]);
 gulp.task("lint", ["js-lint"]);
 
 gulp.task("js-lint", function() {
-    return gulp.src(all)
-        .pipe(plugins.eslint())
-        .pipe(plugins.eslint.format("unix"))
-        .pipe(plugins.eslint.failAfterError());
+	return gulp.src(all)
+		.pipe(plugins.eslint())
+		.pipe(plugins.eslint.format("unix"))
+		.pipe(plugins.eslint.failAfterError());
 });
 
 // Test
 
 gulp.task("test", function(done) {
-    new Server({
-        configFile: __dirname + "/karma.conf.js",
-        singleRun: true
-    }, done).start();
+	new Server({
+		configFile: __dirname + "/karma.conf.js",
+		singleRun: true
+	}, done).start();
 });
 
 //
 // Deploy
 //
 
-// Todo: minify
-gulp.task("build", function() {
-    gulp.src(sources)
-        .pipe(plugins.stripCode({
-            start_comment: "start-test",
-            end_comment: "end-test"
-        }))
-        .pipe(gulp.dest("dis"));
+gulp.task("strip", function() {
+	return gulp.src(sources)
+		.pipe(plugins.stripCode({
+			start_comment: "start-test",
+			end_comment: "end-test"
+		}))
+		.pipe(gulp.dest("strip"));
 });
+
+var requireJSOptions = {
+	mainConfigFile: "./config.js",
+	name: "../node_modules/almond/almond",
+	wrap: true,
+	findNestedDependencies: true
+};
+
+gulp.task("optimize", ["strip"], function() {
+	var options = Object.assign(requireJSOptions);
+	options.optimize = "none";
+	options.include = ["objectjs"];
+	options.insertRequire = ["objectjs"];
+	options.out = "objectjs.js";
+
+	return gulp.src("strip/objectjs.js")
+		.pipe(plugins.optimizer(options))
+		.pipe(gulp.dest("dist"));
+});
+
+gulp.task("optimize:minify", ["strip"], function() {
+	var options = Object.assign(requireJSOptions);
+	delete options.optimize;
+	options.include = ["objectjs"];
+	options.insertRequire = ["objectjs"];
+	options.out = "objectjs.min.js";
+
+	return gulp.src("strip/objectjs.js")
+		.pipe(plugins.optimizer(options))
+		.pipe(gulp.dest("dist"));
+});
+
+gulp.task("build", plugins.sequence(
+	"optimize",
+	"optimize:minify",
+	"clean:strip"
+));
 
 //
 // Clean
 //
 
-gulp.task("clean", function(cb) {
-    del([
-        "dist"
-    ], cb);
+gulp.task("clean:strip", function(cb) {
+	del([
+		"strip"
+	], cb);
 });
+
+gulp.task("clean:build", function(cb) {
+	del([
+		"dist"
+	], cb);
+});
+
+gulp.task("clean", plugins.sequence([
+	"clean:build",
+	"clean:strip"
+]));
