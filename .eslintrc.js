eeb74dc129d4d36d370b164402912fd1119d269e
Damien Cassou <damien@cassou.me>
2023-12-06 17:12:45 +0100

    Simplify .eslintrc.js file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,12 @@
-(function() {
-	let error = 2;
-
-	module.exports = {
-		parserOptions: {
-			"ecmaVersion": 6,
-			"sourceType": "module"
-		},
-		root: true,
-		extends: "ftgp",
-		rules: {
-			"quotes": [error, "double"],
-			"ftgp/require-class-comment": 0
-		}
-	};
-})();
+module.exports = {
+	parserOptions: {
+		"ecmaVersion": 6,
+		"sourceType": "module"
+	},
+	root: true,
+	extends: "ftgp",
+	rules: {
+		"quotes": ["error", "double"],
+		"ftgp/require-class-comment": 0
+	}
+};
